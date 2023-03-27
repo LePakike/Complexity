be2543e95903b6b1c8fb7eaef6f81aacf79fc452
brianmanderson <markba122@gmail.com>
2023-03-27 17:10:36 -0300

    can now perfform ReturnDiff as a difference in locations between leaves

--- src/Complexity.ApertureMetric/Algebra.cs
+++ src/Complexity.ApertureMetric/Algebra.cs
@@ -25,5 +25,30 @@ namespace Complexity.ApertureMetric
         {
             return Enumerable.Range(0, n);
         }
+        public static double[][] ReturnDiff(IEnumerable<Aperture> apertures)
+        {
+            List<List<double>> raveled_apperture_leaf_positions = new List<List<double>>();
+
+            foreach (Aperture aperture in apertures)
+            {
+                List<double> leaf_positions = new List<double>();
+                foreach (LeafPair lp in aperture.LeafPairs)
+                {
+                    leaf_positions.Add(lp.Left);
+                    leaf_positions.Add(lp.Right);
+                }
+                raveled_apperture_leaf_positions.Add(leaf_positions);
+            }
+
+            double[][] diff_in_positions = new double[raveled_apperture_leaf_positions.Count - 1][];
+            for (int i = 0, j = raveled_apperture_leaf_positions.Count - 1; i < j; i++)
+            {
+                List<double> first_position = raveled_apperture_leaf_positions[i];
+                List<double> second_position = raveled_apperture_leaf_positions[i + 1];
+
+                diff_in_positions[i] = first_position.Zip(second_position, (x, y) => x - y).ToArray();
+            }
+            return diff_in_positions;
+        }
     }
 }
