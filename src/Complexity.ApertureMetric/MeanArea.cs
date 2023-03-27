a3ce7463bc0ca974be1ae2f88a5102b0d85c5061
brianmanderson <markba122@gmail.com>
2023-03-27 11:36:00 -0300

    added area metric, just testing what I can change

---
+++ src/Complexity.ApertureMetric/MeanArea.cs
@@ -0,0 +1,15 @@
+﻿namespace Complexity.ApertureMetric
+{
+    public class AreaMetric
+    {
+        public double Calculate(Aperture aperture)
+        {
+            return aperture.Area();
+        }
+
+        public static double DivisionOrDefault(double a, double b)
+        {
+            return (b != 0.0) ? (a / b) : 0.0;
+        }
+    }
+}
